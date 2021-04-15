0e5608dfe1a3af737201c60e9d8c28782a66cbd1
Archan Patkar <pat@archanpatkar.com>
2021-04-15 08:43:53 +0530

    placeholder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 // Placeholder
-function unitpropgation(phi) {
-
+function unitpropgation(l, clauses) {
+    // todo
 }
 
-function dpll(clauses) {
+function isunit(clause) {
+    // todo
+}
 
+function ispure(clause) {
+    // todo 
+}
+
+function pureliteralassign(l, clauses) {
+    // todo
+}
+
+function chooseliteral(clauses) {
+    // todo
+}
+
+function replaceVar(l,val,clauses) {
+    // todo
+}
+
+const empty = new Set(null);
+
+function dpll(clauses) {
+    if(clauses.reduce((acc,v) => acc + v, 0)) return true;
+    else if(clauses.includes(empty)) return false;
+    const uc = clauses.filter(isunit);
+    for(let l of uc) clauses = unitpropgation(l,clauses);
+    const pl = clauses.filter(ispure);
+    for(let l of pl) clauses = pureliteralassign(l,clauses);
+    const l = chooseliteral(clauses);
+    return dpll(replaceVar(l,true,clauses)) || dpll(replaceVar(l,false,clauses));
 }
